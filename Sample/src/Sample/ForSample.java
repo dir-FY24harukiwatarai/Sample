a2cc5f3128c1b45b4c552e369af130eea0c8dcce
dir-FY24harukiwatarai <haruki.watarai.k215@dirbato.co.jp>
2024-04-05 17:11:41 +0900

    chapter2終わり 例外処理のthrowの理解が追いついていない

---
+++ Sample/src/Sample/ForSample.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        for (var i=1; i<=20;i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
---
+++ Sample/src/Sample/MethodSample.java
@@ -0,0 +1,20 @@
+package Sample;
+
+public class MethodSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        var number1 = 100;
+        var number2 = 10;
+        sumMethod(number1,number2);
+
+    }
+
+
+
+public static void sumMethod(int num1,int num2) {
+    var result = num1 +num2;
+    System.out.println("sumMethodの結果は"+result);
+    
+}
+}
